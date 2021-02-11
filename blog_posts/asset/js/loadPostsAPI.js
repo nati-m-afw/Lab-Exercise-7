67fd269b1c42013d6025e199d7e8173e043a6e26
nati-m <it.natnael.mekonnen@gmail.com>
2021-02-12 00:54:48 +0300

    added search functionality; disabled search while loading

--- blog_posts/asset/js/loadPostsAPI.js
+++ blog_posts/asset/js/loadPostsAPI.js
@@ -1,6 +1,7 @@
 // UI Vars 
 const postDiv3 = document.getElementById('thePosts');
 const loader = document.getElementById('loader');
+const filter = document.getElementById('filter');
 
 //Load Every thing ....
 document.addEventListener("DOMContentLoaded", () => {
@@ -17,30 +18,45 @@ function load_fromPlaceHolder() {
     .then((posts) => {
         let output = '';
         loader.style.display = 'none';
+        filter.removeAttribute('disabled');
         posts.forEach(function(post) {
             output += `
-                <div class="ui divided items" id="myPost">
                 <div class="item">
-                <div class="image"> <img src="https://picsum.photos/300/200"> </div>
-                <div class="content">
-                <a class="header" href="#" id="bTitle"> ${post.title} </a>
-                <div class="meta">
-                <span id="bDate">${post.date} </span>
-                <span>By: <a href="#" id="bAuthor"> ${post.userId}</a></span>
+                    <div class="image"> <img src="https://picsum.photos/300/200"> </div>
+                    <div class="content">
+                        <a class="header" href="#" id="bTitle"> ${post.title} </a>
+                        <div class="meta">
+                            <span id="bDate">${new Date(new Date().getTime()).toLocaleString('en-US')} </span>
+                            <span>By: <a href="#" id="bAuthor"> ${post.userId}</a></span>
+                        </div>
+                        <div class="description"> <p id="bDesc"> ${post.body} </p> </div>
+                        <div class="extra"> <a class="ui floated basic violet button" href="#">Read Mores</a> </div>
+                    </div>
                 </div>
-                <div class="description"> <p id="bDesc"> ${post.body} </p </div>
-                <div class="extra"> <a class="ui floated basic violet button" href="#">Read Mores</a> </div>
-                </div>
-                </div>
-                </div> `;
+                `;
                 //same as previous code
                 output += ` `; // same code as previous with few update
         });
+        console.log(output);
         postDiv3.innerHTML += output;
     })
     .catch((err) => { console.log(err); })
 
 }
+
+filter.addEventListener('keyup', filterTasks);
+function filterTasks(e){
+    for(let i = 0; i < postDiv3.childElementCount; i++){
+        if (postDiv3.children[i].lastElementChild.firstElementChild.textContent.indexOf(filter.value) != -1){
+            postDiv3.children[i].style.display = 'flex';
+        }
+        else{
+            postDiv3.children[i].style.display = 'none';
+        }
+    }
+}
+
+
 //async await
 function load_fromPlaceHolder_new() {
 
